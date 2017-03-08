8a216b35121d5b20f98423ce0ffcb9bd249a7b56
intenter <int@intenter.org>
2017-03-07 20:54:26 -0600

    Added err function. Fetching from remotes is a separate function now

--- git_sync.sh
+++ git_sync.sh
@@ -9,6 +9,24 @@
 #Configuration
 WRKDIR=/Volumes/MacHD/Users/intmac/projects/git_sync_wrk
 
+# prints error message
+err() {
+	echo "ERROR: $1"
+}
+
+# fetch from remote with prune option to get rid of deleted branches
+fetch_remote() {
+		REPO=$1
+		REMOTE=$2
+		cd "$WRKDIR/$REPO"
+		echo "Fetching $REPO/$REMOTE..."
+		git fetch -p "$REMOTE"
+		if [[ $? != 0 ]]; then
+			err "Can't fetch from $REPO/$REMOTE"
+		fi
+}
+
+# syncs branches. You can use wildcard
 # $1 - repo, $2 - remote from, $3 remote to, $4 - branch
 sync_branches() {
 	REPO=$1
@@ -22,6 +40,7 @@ sync_branches() {
 	done
 }
 
+# syncs a single branch
 # $1 - repo, $2 - remote from, $3 remote to, $4 - branch
 sync_branch() {
 	REPO=$1
@@ -32,8 +51,6 @@ sync_branch() {
 	echo "Running sync on $REPO: $FROM/$BRANCH => $TO/$BRANCH"
 	cd "$WRKDIR/$REPO"
 	#todo: check repo status here and reset if needed
-	#fetch from remote with prune option to get rid of deleted branches
-	git fetch -p "$FROM"
 	#check if local branch exists
 	git rev-parse --verify --quiet "$BRANCH"
 	if [[ $? != 0 ]]; then
@@ -44,14 +61,17 @@ sync_branch() {
 	git checkout "$BRANCH"
 	git pull --ff-only "$FROM" "$BRANCH"
 	if [[ $? != 0 ]]; then
-		echo "ERROR: Couldn't pull changes from source. Expected to be fast forward pull. Check for local changes."
+		err "Couldn't pull changes from source. Expected to be fast forward pull. Check for local changes."
 	else
 		git push "$TO" "$BRANCH"
 		if [[ $? != 0 ]]; then
-			echo "ERROR: Couldn't push branch $BRANCH to $TO"
+			err "Couldn't push branch $BRANCH to $TO"
 		fi
 	fi
 }
+
+fetch_remote "test_repo1" "origin"
+fetch_remote "test_repo1" "repo2"
 
 sync_branch "test_repo1" "origin" "repo2" "master"
 sync_branches "test_repo1" "repo2" "origin" "test*"
